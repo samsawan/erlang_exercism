6d97aec188818a9fe3169022eddd6ab5cbe59ba5
Sam Sawan <ssawan@onshift.com>
2018-02-13 17:46:52 -0500

    list comprehension

--- strain/src/strain.erl
+++ strain/src/strain.erl
@@ -2,23 +2,8 @@
 
 -export([keep/2, discard/2, test_version/0]).
 
-keep(Fn, List) -> keep(Fn, List, []).
+keep(Fn, List) -> [X || X <- List, Fn(X)].
 
-
-discard(Fn, List) -> discard(Fn, List, []).
+discard(Fn, List) -> [X || X <- List, not Fn(X)].
 
 test_version() -> 1.
-
-keep(_, [], Acc) -> Acc;
-keep(Fn, [H | T], Acc) ->
-	case Fn(H) of
-		true -> keep(Fn, T, Acc ++ [H]);
-		false -> keep(Fn, T, Acc)
-	end.
-
-discard(_, [], Acc) -> Acc;
-discard(Fn, [H | T], Acc) ->
-	case Fn(H) of
-		false -> discard(Fn, T, Acc ++ [H]);
-		true -> discard(Fn, T, Acc)
-	end.

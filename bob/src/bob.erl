0b2d8d21eb24fdc4b80f1a0024dcb84101e8a379
Sam Sawan <ssawan@onshift.com>
2018-02-13 17:29:33 -0500

    handle silence

--- bob/src/bob.erl
+++ bob/src/bob.erl
@@ -2,6 +2,7 @@
 
 -export([response/1, test_version/0]).
 
+response([]) -> "Fine. Be that way!";
 response(String) ->
 	Response =
 	lists:foldl(fun(TransformFunc, BobResponse) ->
@@ -12,6 +13,7 @@ response(String) ->
 	end,
 	{false, String},
 	[
+		fun check_for_silence/1,
 		fun check_for_shouting/1,
 		fun check_for_question/1
 	]),
@@ -22,14 +24,25 @@ response(String) ->
 
 test_version() -> 2.
 
+check_for_silence(String) ->
+	A = clear_whitespace(String),
+	io:format("samsawan 	HERES A: ~p", [A]),
+	case A of
+		"" -> {true, "Fine. Be that way!"};
+		_ -> {false, String}
+	end.
+
 check_for_question(String) ->
-	case hd(lists:reverse(String)) =:= $? of
+	case hd(lists:reverse(string:strip(String))) =:= $? of
 		true -> {true, "Sure."};
 		false -> {false, String}
 	end.
 
 check_for_shouting(String) ->
-	case string:to_upper(String) =:= String of
+	case string:to_upper(String) =:= String andalso re:run(String, <<"[A-Z]">>) =/= nomatch of
 		true -> {true, "Whoa, chill out!"};
 		false -> {false, String}
 	end.
+
+clear_whitespace(String) ->
+	lists:filter(fun(Char) -> not lists:member(Char, "\n \r\t") end, String).

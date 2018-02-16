120ac16ff046ea28bfe58406ef176857c9d3eae1
Sam Sawan <ssawan@onshift.com>
2018-02-16 16:54:50 -0500

    solve difference of squares

---
+++ difference-of-squares/src/difference_of_squares.erl
@@ -0,0 +1,11 @@
+-module(difference_of_squares).
+
+-export([sum_of_squares/1, square_of_sums/1, difference_of_squares/1, test_version/0]).
+
+sum_of_squares(N) -> lists:sum([trunc(math:pow(Num, 2)) || Num <- lists:seq(1, N)]).
+
+square_of_sums(N) -> trunc(math:pow(lists:sum(lists:seq(1, N)), 2)).
+
+difference_of_squares(N) -> square_of_sums(N) - sum_of_squares(N).
+
+test_version() -> 1.

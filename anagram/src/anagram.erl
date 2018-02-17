c48f811a8e9533aa71f45429cf53a436ddde1288
Sam Sawan <ssawan@onshift.com>
2018-02-16 19:04:33 -0500

    utilize sort

--- anagram/src/anagram.erl
+++ anagram/src/anagram.erl
@@ -6,7 +6,8 @@ find(Word, Candidates) ->
 	LowerWord = string:to_lower(Word),
 	lists:foldl(
 		fun(Candidate, Anagrams) ->
-			case is_anagram(LowerWord, string:to_lower(Candidate)) of
+			LowerCandidate = string:to_lower(Candidate),
+			case not (LowerCandidate =:= LowerWord) andalso (lists:sort(LowerCandidate) =:= lists:sort(LowerWord)) of
 				true -> Anagrams ++ [Candidate];
 				false -> Anagrams
 			end
@@ -15,11 +16,3 @@ find(Word, Candidates) ->
 	Candidates).
 
 test_version() -> 1.
-
-is_anagram(_Word, _Word) -> false;
-is_anagram(Word, Candidate) when length(Word) =/= length(Candidate) -> false;
-is_anagram(Word, Candidate) ->
-	case lists:foldl(fun(Char, RemainingCandidate) -> lists:delete(Char, RemainingCandidate) end, Candidate, Word) of
-		"" -> true;
-		_ -> false
-	end.

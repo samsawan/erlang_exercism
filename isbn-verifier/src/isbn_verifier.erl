44b912d6c643721271d908c8e1e8c99b7094d123
Sam Sawan <ssawan@onshift.com>
2018-05-05 11:24:07 -0400

    shitty soln to isbn

--- isbn-verifier/src/isbn_verifier.erl
+++ isbn-verifier/src/isbn_verifier.erl
@@ -3,15 +3,23 @@
 -export([is_valid/1, test_version/0]).
 
 is_valid(Isbn) ->
+	FindValFunc =
+	fun($X) -> 10;
+	   (C) -> C - $0
+	end,
 	FilterMapFunc =
-		fun(C) ->
-			case (C >= $0 andalso C =< $9) orelse C == $X of
-				true -> {true, C - $0};
-				false -> false
+	fun(C) ->
+		case (C >= $0 andalso C =< $9) orelse C == $X of
+			true -> {true, FindValFunc(C)};
+			false -> false
 		end
 	end,
 	SanitizedIsbn = lists:filtermap(FilterMapFunc, Isbn),
-	ConcatVals = lists:zip(SanitizedIsbn, lists:reverse(lists:seq(1, 10))),
-	lists:foldl(fun({A,B}, Sum) -> A * B + Sum end, 0, ConcatVals) rem 11 == 0.
+	case length(SanitizedIsbn) == 10 of
+		true ->
+			ConcatVals = lists:zip(SanitizedIsbn, lists:reverse(lists:seq(1, 10))),
+			lists:foldl(fun({A,B}, Sum) -> A * B + Sum end, 0, ConcatVals) rem 11 == 0;
+		false -> false
+	end.
 
 test_version() -> 1.
